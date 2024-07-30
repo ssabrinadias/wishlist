66bfb7aabc4d1d80ba49cd96b22d9dd1973570c5
Sabrina Dias <dias.sabrina@gmail.com>
2024-07-30 12:14:50 -0300

    fix: layout fix

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -10,58 +10,63 @@ const Header = () => {
     window.location.href = url;
   };
 
+  const logo = (device: string) => {
+    const isMobile = device === 'mobile';
+    return (
+      <Image
+        src={netshoesLogo}
+        alt="Netshoes Logo"
+        width={isMobile ? 150 : 200}
+        height={isMobile ? 19.5 : 25.2}
+      />
+    );
+  };
+
+  const mobileView = (
+    <div className="flex xl:hidden justify-between w-full px-4">
+      <FaUserCircle className="text-white h-8 w-8" />
+      <div className="flex-1 flex justify-center ">
+        <button onClick={() => handleNavigation('/')}>{logo('desktop')}</button>
+      </div>
+      <div
+        className="cursor-pointer "
+        onClick={() => handleNavigation('/wishlist')}
+      >
+        <FaHeart className="text-white h-8 w-8" />
+      </div>
+    </div>
+  );
+
+  const desktopView = (
+    <div className="hidden xl:flex justify-between max-w-1132px mx-auto w-full">
+      <div
+        className="mr-20 cursor-pointer"
+        onClick={() => handleNavigation('/')}
+      >
+        <button className="flex items-center">{logo('desktop')}</button>
+      </div>
+
+      <div className="flex items-center space-x-4 mr-7">
+        <div
+          className="mr-20 flex items-center text-white cursor-pointer"
+          onClick={() => handleNavigation('/wishlist')}
+          data-id="wishlist-link"
+        >
+          <FaHeart className="mr-2" />
+          <span>Wishlist</span>
+        </div>
+        <div className="relative group">
+          <FaUserCircle className="text-white h-8 w-8" />
+          <MenuDropdown />
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <header className="bg-purple-700 flex justify-between items-center max-w-full mx-auto w-full h-12 sm:h-[60px] xl:h-[90px]">
-      <div className="hidden xl:flex justify-between max-w-1132px mx-auto w-full">
-        <div
-          className="mr-20 cursor-pointer"
-          onClick={() => handleNavigation('/')}
-        >
-          <div className="flex items-center">
-            <Image
-              src={netshoesLogo}
-              alt="Netshoes Logo"
-              width={200}
-              height={25.5}
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center space-x-4 mr-7">
-          <div
-            className="mr-20 flex items-center text-white cursor-pointer"
-            onClick={() => handleNavigation('/wishlist')}
-            data-id="wishlist-link"
-          >
-            <FaHeart className="mr-2" />
-            <span>Wishlist</span>
-          </div>
-          <div className="relative group">
-            <FaUserCircle className="text-white h-8 w-8" />
-            <MenuDropdown />
-          </div>
-        </div>
-      </div>
-      <div className="flex xl:hidden justify-between w-full px-4">
-        <FaUserCircle className="text-white h-8 w-8" />
-        <div
-          className="flex-1 flex justify-center min-h-[150] cursor-pointer"
-          onClick={() => handleNavigation('/')}
-        >
-          <Image
-            src={netshoesLogo}
-            alt="Netshoes Logo"
-            width={150}
-            height={19.5}
-          />
-        </div>
-        <div
-          className="mr-20 cursor-pointer"
-          onClick={() => handleNavigation('/wishlist')}
-        >
-          <FaHeart className="text-white h-8 w-8" />
-        </div>
-      </div>
+      {desktopView}
+      {mobileView}
     </header>
   );
 };
